37bb96bd1ccc97098ff47033fcc75ee5ead28ca7
Jody Duke <contact@jodyduke.co.uk>
2018-06-13 22:48:29 +0100

    added calendar to rootreducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import currentModal from './modal_filter'
+import currentCalendar from './calendar';
 
 export default combineReducers({
+    currentCalendar,
     currentModal
 })
