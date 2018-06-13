41aaf6d223958f8168ed786f2added8ab4cf95da
Jody Duke <contact@jodyduke.co.uk>
2018-06-13 22:46:22 +0100

    added map to calendar component for weekdays and nodes

--- src/components/calendar/calendar.js
+++ src/components/calendar/calendar.js
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { days } from '../../utils/days_of_the_week';
 
-const Calendar = (props) => {
+const mapStateToProps = state => ({
+    store: state.currentCalendar
+})
+
+const CalendarNode = (props) => {
     return (
-        <div className={props.styles}>calendar</div>
+        <div className={props.styles}>
+            {props.number}
+        </div>
     )
 }
+
+class ConnectedCalendar extends Component {
+    render() {
+        return (
+            <div className="calendar-container">
+                {days.map((e, k) => {
+                    return <div key={k} className="calendar-weekdays">{e}</div>
+                })}
+                {this.props.store.calendar.map((e, k) => {
+                    if(Object.values(e)[0] === true) {
+                        return <CalendarNode key={k} styles="calendar-node available" number={Object.keys(e)} />
+                    }
+
+                    return <CalendarNode key={k} styles="calendar-node unavailable" number={Object.keys(e)}/>
+
+                })}
+            </div>
+        )
+    }
+}
+
+
+const Calendar = connect(mapStateToProps)(ConnectedCalendar)
 
 export default Calendar
