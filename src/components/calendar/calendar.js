b9c9f8b91eaabe89a0cfbd284321c09066e4b7ba
Jody Duke <contact@jodyduke.co.uk>
2018-06-13 23:11:20 +0100

    added dispatch and handleClick handlers to calender

--- src/components/calendar/calendar.js
+++ src/components/calendar/calendar.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { days } from '../../utils/days_of_the_week';
+import { changeDay } from '../../redux/actions'
 
 const mapStateToProps = state => ({
     store: state.currentCalendar
+})
+
+const mapDispatchToProps = dispatch => ({
+    changeDay: int => dispatch(changeDay(int))
 })
 
 const CalendarNode = (props) => {
@@ -15,14 +20,29 @@ const CalendarNode = (props) => {
 }
 
 class ConnectedCalendar extends Component {
+    constructor(props) {
+        super(props)
+
+        this.handleClick = this.handleClick.bind(this)
+    }
+
+    handleClick(e) {
+        if(e.target.className === "calendar-node available") {
+            this.props.changeDay(parseInt(e.target.innerHTML, 10))
+        }
+    }
+
     render() {
         return (
-            <div className="calendar-container">
+            <div onClick={this.handleClick} className="calendar-container">
                 {days.map((e, k) => {
                     return <div key={k} className="calendar-weekdays">{e}</div>
                 })}
                 {this.props.store.calendar.map((e, k) => {
-                    if(Object.values(e)[0] === true) {
+                    if (Object.keys(e)[0] == this.props.store.activeDay) {
+                        return <CalendarNode key={k} styles="calendar-node active" number={Object.keys(e)} />                        
+                    }
+                    else if(Object.values(e)[0] === true) {
                         return <CalendarNode key={k} styles="calendar-node available" number={Object.keys(e)} />
                     }
 
@@ -35,6 +55,6 @@ class ConnectedCalendar extends Component {
 }
 
 
-const Calendar = connect(mapStateToProps)(ConnectedCalendar)
+const Calendar = connect(mapStateToProps, mapDispatchToProps)(ConnectedCalendar)
 
 export default Calendar
