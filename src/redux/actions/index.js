3526aa2896939519f2736e57f2785da9f9997ac8
Jody Duke <contact@jodyduke.co.uk>
2018-06-13 22:48:09 +0100

    added changeDay action

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,6 +1,11 @@
-import { CURRENT_MODAL } from "../constants";
+import { CURRENT_MODAL, CHANGE_DAY } from "../constants";
 
 export const currentModal = modal => ({
     type: CURRENT_MODAL,
     modal
+})
+
+export const changeDay = int => ({
+    type: CHANGE_DAY,
+    int
 })
