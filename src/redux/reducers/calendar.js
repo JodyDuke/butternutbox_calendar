d840ae41fd1ec6740888980a0ba30e157ea78e14
Jody Duke <contact@jodyduke.co.uk>
2018-06-13 22:47:50 +0100

    added calendar reducer and initial state

---
+++ src/redux/reducers/calendar.js
@@ -0,0 +1,50 @@
+import { CHANGE_DAY } from '../constants'
+
+const initialState = {
+    activeDay: 6,
+    calendar: [
+        {},
+        {1: false},
+        {2: true},
+        {3: true},
+        {4: false},
+        {5: false},
+        {6: true},
+        {7: true},
+        {8: false},
+        {9: true},
+        {10: true},
+        {11: false},
+        {12: false},
+        {13: true},
+        {14: true},
+        {15: false},
+        {16: true},
+        {17: true},
+        {18: false},
+        {19: false},
+        {20: true},
+        {21: true},
+        {22: false},
+        {23: true},
+        {24: true},
+        {25: false},
+        {26: false},
+        {27: true},
+        {28: true},
+        {29: false},
+        {30: true},
+        {31: true}
+    ]
+}
+
+const currentCalendar = (state = initialState, action) => {
+    switch(action.type){
+        case CHANGE_DAY:
+            return {...state, activeDay: action.payload}
+        default: 
+            return state
+    }
+}
+
+export default currentCalendar
